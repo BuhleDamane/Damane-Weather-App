38405a7b4ecd082eb61424052df9d0687cdc29a1
BuhleDamane <ziziphodamane@gmail.com>
2024-01-24 01:46:58 +0200

    created a new function and introduced the API .

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ function displayTemperature(response) {
     temperatureElement.innerHTML = Math.round(temperature);
     humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`
+
+    getForecast(response.data.city);
   }
 
   function formatDate(date) {
@@ -58,7 +60,14 @@ function handleSearchSubmit(event) {
     
     searchCity(searchInput.value)
 }
-function displayForecast (){
+function getForecast(city){
+    let apiKey = "13d2ee51f9b4o75695t30ad14de90637";
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`
+//console.log(apiUrl);
+axios.get(apiUrl).then(displayForecast);
+}
+function displayForecast (response){
+    console.log(response.data);
     let forecastElement = document.querySelector("#forecast")
  
     let days = [`Tue`, `Wed`, `Thu`, `Fri`, `Sat`]
